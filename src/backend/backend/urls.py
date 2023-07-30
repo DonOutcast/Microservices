9364d71f770075c18c0ba9fc245dc4d37a18163a
DonOutcast <sham1996@yandex.ru>
2023-07-30 12:23:49 +0300

    added languages

--- src/backend/backend/urls.py
+++ src/backend/backend/urls.py
@@ -1,7 +1,8 @@
-"""django_movies URL Configuration
+"""
+URL configuration for backend project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.1/topics/http/urls/
+    https://docs.djangoproject.com/en/4.2/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -14,8 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf.urls.i18n import i18n_patterns
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("i18n/", include("django.conf.urls.i18n")),
 ]
+urlpatterns += i18n_patterns(path("admin/", admin.site.urls))
